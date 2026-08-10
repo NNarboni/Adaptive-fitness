5dc4f268cd9042e67b936c02b9b491515d7f0554
NNarboni <94265403+NNarboni@users.noreply.github.com>
2026-08-10 18:45:37 -0400

    Update start.js

--- api/whoop/start.js
+++ api/whoop/start.js
@@ -1,9 +1,12 @@
-import crypto from "crypto";
+const crypto = require("crypto");
 
-export default function handler(req,res){
-  const state=crypto.randomBytes(12).toString("hex");
- const redirectUri="https://adaptivefitnesswhoop.vercel.app/api/whoop/callback ;
-  const scope=[
+module.exports = function handler(req, res) {
+  const state = crypto.randomBytes(12).toString("hex");
+
+  const redirectUri =
+    "https://adaptivefitnesswhoop.vercel.app/api/whoop/callback";
+
+  const scope = [
     "offline",
     "read:recovery",
     "read:cycles",
@@ -11,11 +14,35 @@ export default function handler(req,res){
     "read:workout",
     "read:body_measurement"
   ].join(" ");
-  const u=new URL("https://api.prod.whoop.com/oauth/oauth2/auth");
-  u.searchParams.set("client_id",process.env.WHOOP_CLIENT_ID||"");
-  u.searchParams.set("redirect_uri",redirectUri);
-  u.searchParams.set("response_type","code");
-  u.searchParams.set("scope",scope);
-  u.searchParams.set("state",state);
+
+  const u = new URL(
+    "https://api.prod.whoop.com/oauth/oauth2/auth"
+  );
+
+  u.searchParams.set(
+    "client_id",
+    process.env.WHOOP_CLIENT_ID || ""
+  );
+
+  u.searchParams.set(
+    "redirect_uri",
+    redirectUri
+  );
+
+  u.searchParams.set(
+    "response_type",
+    "code"
+  );
+
+  u.searchParams.set(
+    "scope",
+    scope
+  );
+
+  u.searchParams.set(
+    "state",
+    state
+  );
+
   res.redirect(u.toString());
-}
+};
